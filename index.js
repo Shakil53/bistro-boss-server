887d3c9c47ee5ab92ecbdd292b7b28702e144917
Shakil53 <shakilahmed.dcc@gmail.com>
2023-09-14 00:03:52 +0600

    cart apis added api call

--- index.js
+++ index.js
@@ -39,7 +39,20 @@ async function run() {
         })
 
 
-        // Cart collection
+        // Cart collection & apis
+        app.get('/carts', async (req, res) => {
+            const email = req.query.email;
+            // console.log(email)
+            if (!email) {
+                res.send([])
+            }
+            else {
+                const query = { email: email };
+                const result = await cartCollection.find(query).toArray();
+                res.send(result)
+            }
+        })
+
         app.post('/carts', async (req, res) => {
             const item = req.body;
             console.log(item);
