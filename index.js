7051e5bdfeeb05f582ce91512d877a643b5d5a70
Shakil53 <shakilahmed.dcc@gmail.com>
2023-09-15 10:11:19 +0600

    update user beside create deleted button and deleted successfully

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -56,6 +57,32 @@ async function run() {
         })
 
 
+        // update Users
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result)
+        })
+        // users delete
+        app.delete('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            // console.log(id)
+            const query = { _id: new ObjectId(id) };
+            const result = await cartCollection.deleteOne(query);
+            res.send(result)
+
+
+        })
+
+
+
+
 
         // Menu APis
         app.get('/menu', async (req, res) => {
@@ -79,7 +106,7 @@ async function run() {
 
         app.post('/carts', async (req, res) => {
             const carts = req.body;
-            console.log(carts);
+            // console.log(carts);
             const result = await cartCollection.insertOne(carts);
             res.send(result)
         })
