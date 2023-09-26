94ce53b71831122d75dff44875c092454b8691db
Shakil53 <shakilahmed.dcc@gmail.com>
2023-09-26 11:01:58 +0600

    manageItem create into dashboard and delete item done

--- index.js
+++ index.js
@@ -100,10 +100,18 @@ async function run() {
             res.send(result)
         })
 
+
         app.post('/menu', async (req, res) => {
             const newItem = req.body;
             const result = await menuCollection.insertOne(newItem)
             res.send(result)
+        })
+
+        app.delete('/menu/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await menuCollection.deleteOne(query)
+            res.send(result);
         })
 
 
